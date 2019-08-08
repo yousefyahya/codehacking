a9b029cff9c925efb0c106eceba914a90a4e64b6
yousef yahya <yousef.yahya39@gmail.com>
2019-08-08 16:48:45 +0300

    Just start working on Comments & Replies Section

---
+++ app/Comment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    //
+    protected $fillable = ['post_id', 'is_active', 'author', 'email', 'body'];
+
+    public function post(){
+        return $this->belongsTo('App\Post');
+    }
+
+    public function replies(){
+        return $this->hasMany('App\CommentReply');
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,4 +28,6 @@ Route::group(['middleware'=>'admin'], function () {
     Route::resource('/admin/posts', 'AdminPostsController');
     Route::resource('/admin/categories', 'AdminCategoriesController');
     Route::resource('/admin/media', 'AdminMediaController');
+    Route::resource('/admin/comments', 'PostCommentsController');
+    Route::resource('/admin/comment/replies', 'CommentRepliesController');
 });
